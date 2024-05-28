9351b02dd4ed45949d86090e28146d9c16448c8e
zendayat <nmapzendaya@gmail.com>
2024-05-28 16:29:23 +0400

    update: adding components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Highlights from './components/Highlights';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Model from './components/Model';
+import Features from './components/Features';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Highlights/>
+      <Model/>
+      <Features/>
       
     </main>
   )
