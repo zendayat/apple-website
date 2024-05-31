5f9d52c234fe25ce52def39b2fe031acd2551d41
zendayat <nmapzendaya@gmail.com>
2024-05-31 08:04:48 +0400

    update: intergrating footer component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Model/>
       <Features/>
       <HowItWorks/>
+      <Footer/>
       
     </main>
   )
